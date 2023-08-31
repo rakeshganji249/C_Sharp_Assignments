8df0ce5a1935ace1fd26ec8f6af81452415be40b
rakesh <rakeshganji467@gmail.com>
2023-08-31 11:49:32 +0530

    Assignment 5

---
+++ OutParameterBehaviour/OutParameterBehaviour/Program.cs
@@ -0,0 +1,32 @@
+﻿
+using System;
+using System.Threading;
+namespace OutParameterBehaviour
+{
+    class Program
+    {
+        public int value;
+        /// <summary>
+        /// Method to check the behaviour of out parameter
+        /// </summary>
+        /// <param name="x"></param>
+        public static void CheckBehaviour(out int value)
+        {
+            //x = 2;
+            Console.WriteLine("method Initiated");
+            Thread.Sleep(3000);
+            value = 3;
+        }
+
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            Thread thread = new Thread(() => CheckBehaviour(out program.value));
+            thread.Start();
+            Thread.Sleep(2000);
+            Console.WriteLine(program.value);
+            thread.Join();
+
+        }
+    }
+}
